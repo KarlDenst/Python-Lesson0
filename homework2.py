eb3fd37cf01e5e86ef50a205fc840bb00be1f146
KarlDenst <skylimit@mail.ru>
2024-07-01 20:46:19 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homewrk_count=12
+hours_count=1.5
+course_name='Python'
+
+print('Курс:',course_name,','' всего задач:',homewrk_count,','' затрачено часов:',hours_count,',','среднее время выполнения',hours_count/homewrk_count)
