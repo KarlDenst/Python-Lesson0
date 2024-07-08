897f2ca13909f107741d377ccbde53af4e43a886
KarlDenst <skylimit@mail.ru>
2024-07-08 19:25:41 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -12,11 +12,21 @@ student_ave_marks={students[0]:sum(grades[0])/len(grades[0]),
                    students[4]:sum(grades[4])/len(grades[4])}
 print(student_ave_marks)
 
+# или так
+
+ave_marks=[sum(grades[0])/len(grades[0]),
+                   sum(grades[1])/len(grades[1]),
+                   sum(grades[2])/len(grades[2]),
+                   sum(grades[3])/len(grades[3]),
+                   sum(grades[4])/len(grades[4])]
+
+new_student_ave_marks=dict(zip(students,ave_marks))
+print(new_student_ave_marks)
 
 
 #интерфейс для учителя
 
-#name=input('Введите имя студента: ')
-#print('Средний балл студента',name,student_ave_marks[name])
+name=input('Введите имя студента: ')
+print('Средний балл студента',name,new_student_ave_marks[name])
 
 
