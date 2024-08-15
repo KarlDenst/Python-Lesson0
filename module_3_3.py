05d170539caa88657671480341ac8669191b2dba
KarlDenst <skylimit@mail.ru>
2024-08-15 22:31:55 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -7,7 +7,8 @@ print_params()
 print_params('point')
 print_params(2,0)
 print_params('point', [23,111,8], False)
-#print_params('point', [23,111,8], False, 'test') - не будет работать, тк задано больше аргументов, чем может принять функция
+
+# print_params('point', [23,111,8], False, 'test') - не будет работать, тк задано больше аргументов, чем может принять функция
 
 print_params(b=25)
 print_params(c=[1,2,3])
@@ -18,6 +19,7 @@ values_dict = {'a': 'zero', 'b': [1, 'meinheim', False], 'c': 555}
 
 print_params(*values_list)
 print_params(**values_dict)
+
 # print_params(*values_list, **values_dict) # - не будет работать, тк сумма элементов списка и словаря > кол-ва аргументов в функции
 
 
