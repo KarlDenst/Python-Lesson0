1e995addc1743802ee12928676c4243c6ee0e6ab
KarlDenst <skylimit@mail.ru>
2024-08-16 13:58:52 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for i in range(len(other_words)):
+        other_words = list(other_words)
+        if root_word.lower() in other_words[i].lower():
+            same_words.append(other_words[i])
+        elif other_words[i].lower() in root_word.lower():
+            same_words.append(other_words[i])
+    print(same_words)
+
+result1 = single_root_words('rich', 'richest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+
